5e7a8d635f400af47bc7c8f49e904ea1da1fb793
sayed.riaydh <sayed.riaydh@raremark.com>
2020-12-09 22:33:00 +0600

    android setup complete

--- android/app/src/main/java/com/mypt/MainApplication.java
+++ android/app/src/main/java/com/mypt/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           new SplashScreenReactPackage();
+          new RNI18nPackage();
           return packages;
         }
 
